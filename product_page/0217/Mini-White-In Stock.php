9788000e1e60912f8c4e45b7cb4822f67079462e
InternshipJubilee <126126166+InternshipJubilee@users.noreply.github.com>
2023-05-04 17:17:38 -0700

    Add files via upload

---
+++ product_page/0217/Mini-White-In Stock.php
@@ -0,0 +1,34 @@
+
+<!-- TABS -->
+<h5>Description</h5>
+<h2 style="text-align: center;" data-mce-style="text-align: center;">Smart Coffee Table Classic | Futuristic Furniture</h2>
+<p>This Steinfield Tech Smart Coffee Table Classic is designed to enhance the enjoyment and the usage of the same amount of space in your living room. With two refrigerator drawers, USB ports, fast wireless charging. It is not simply a furniture item, this Smart Coffee Table will elevate your living room.</p>
+<p>The Built-in refrigerator 90L drawer holds beer, soda, wine, and more for any occasion. USB ports and fast wireless charging to keep your devices charged by your side without having to leave the conversation or in the middle of a movie. A LED touch control panel is built-in seamlessly into the tempered glass top, easy to access the volume and adjust the temperature of the refrigerator drawer. It is the perfect item for any home.</p>
+<p> </p>
+<ul>
+<li>
+<strong>Overall Dimensions</strong>: 41"L x 23“W x 18"H</li>
+<li>
+<strong>Built-in</strong>: Wireless Charging, USB Charger, and Atmosphere lamp</li>
+<li>
+<strong>Material</strong>: Tempered Glass Top Touch Screen</li>
+<li style="text-align: left;" data-mce-style="text-align: left;"><strong><span style="color: #ff2a00;" data-mce-style="color: #ff2a00;">In-stock items will be shipped out within 1-2 weeks. </span></strong></li>
+<li>US - Nationwide Shipping</li>
+<li>SKU: TB-90</li>
+</ul>
+<p> </p>
+<h5>Key Features</h5>
+<h3>Stay Charged</h3>
+<p>USB charging ports and wireless charging to keep your devices charged by your side without having to leave the conversation or in the middle of a movie.</p>
+<h3>Stay Chill</h3>
+<p>The built-in 90L refrigerator with two separate drawers holds beer, soda, wine, and more. Individual temperature control lets you keep fresh fruits or snacks at the ideal temperature. <span data-mce-fragment="1">Adjustable Refrigerator Temperature Range: 37-54 Degrees Fahrenheit</span>.</p>
+<h3>Smart interface</h3>
+<p>A LED touch control panel is built-in seamlessly into the tempered glass top, easy to access.</p>
+<h3>Tempered Glass Top</h3>
+<p>The round-safe edge design on the corners and the strong and scratch-resistant tempered glass top are perfect for anything you put on top.</p>
+<h3>Energy Efficient</h3>
+<p>Low noise at 40dB running seamlessly in the background and 24h a day with only 0.41Kwh. </p>
+<h5>Dimension</h5>
+<p><img src="https://cdn.shopify.com/s/files/1/0282/9562/6845/files/Dimention-TB90_1f8e9acb-cd33-47c1-9cfc-8e1666aa9f51.jpg?v=1645834804" alt=""></p>
+<ul><!-- /TABS --></ul>
+<button style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;" id="paste-helper-id" data-mce-style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;"></button><button style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;" id="paste-helper-id" data-mce-style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;"></button><button style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;" id="paste-helper-id" data-mce-style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;"></button><button style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;" id="paste-helper-id" data-mce-style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;"></button><button style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;" id="paste-helper-id" data-mce-style="width: 0px; height: 0px; padding: 0px; margin: 0px; border: 0px; display: none;"></button>
